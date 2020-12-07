0e6194743a9255efeb0da089f998c6ff544f6a1d
Tammy Tran <tammy.n.tran@gmail.com>
2020-12-07 21:41:30 +0000

    added function generateRandomString

--- express_server.js
+++ express_server.js
@@ -6,6 +6,14 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  // example given in lecture
+  return Math.random().toString(36).substring(2,8);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,10 +28,15 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-})
+});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -32,6 +45,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
