3b678587bd2a5a286b8d1b96b68af7e1b349b442
Tammy Tran <tammy.n.tran@gmail.com>
2020-12-07 23:15:40 +0000

    added redirecting ability to short url

--- express_server.js
+++ express_server.js
@@ -32,11 +32,16 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -46,11 +51,14 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-})
 
+app.post("/urls", (req, res) => {
+  let newShortURL = generateRandomString();
+  let newLongURL = req.body.longURL;
+  urlDatabase[newShortURL] = newLongURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${newShortURL}`);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
